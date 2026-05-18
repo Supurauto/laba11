75479574b2055a9e0c21a616eea6c58999ecb2d4
dshnmv <dashinimaevaaa555@gmail.com>
2026-05-18 13:51:42 +0700

    исправления

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// Создаём приложение
 const app = createApp(App)
-
-// Регистрируем роутер
 app.use(router)
-
-// Монтируем приложение
 app.mount('#app')
